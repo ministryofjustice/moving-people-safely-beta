ec2b206088d00bdb981a6a1702ab7880e48444e0
Cesidio Di Landa <cesidio.di-landa@digital.justice.gov.uk>
2016-01-29 16:57:17 +0000

    Add a form layer abstraction

---
+++ app/forms/form.rb
@@ -0,0 +1,35 @@
+class Form
+  include Virtus.model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :model
+
+  def initialize(model)
+    @model = model
+    load_model_data
+  end
+
+  def assign_attributes(params)
+    params.each do |key, value|
+      public_send("#{key}=", value)
+    end
+  end
+
+  def save
+    if valid?
+      model.update_attributes(attributes)
+      true
+    else
+      false
+    end
+  end
+
+private
+
+  def load_model_data
+    attributes.each_key do |key|
+      public_send("#{key}=", model.send(key))
+    end
+  end
+end
