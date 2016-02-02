0b2ab12a869571c76ffae6859aa9a36b08459dc1
Dan Palmer <boss@bitrecursive.com>
2016-02-02 23:50:14 +0000

    Integrate age calculation into summary page

--- spec/features/summary_page_spec.rb
+++ spec/features/summary_page_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.feature 'Summary page for an escort' do
   scenario 'viewing an escort records overview' do
-    escort = create(:escort)
-    visit summary_path(escort)
+    travel_to(Date.new(2015, 2, 3)) do
+      escort = create(:escort)
+      visit summary_path(escort)
 
-    expect(page).to have_text('Summary').
-      and have_text('Prisoner Information').
-      and have_link('Edit', href: identification_path(escort)).
-      and have_content('Family name Bigglesworth').
-      and have_content('Forename(s) Tarquin').
-      and have_content('Date of birth 13/02/1972').
-      and have_content('Sex M').
-      and have_content('Nationality British').
-      and have_content('Prison number A1234BC')
+      expect(page).to have_text('Summary').
+        and have_text('Prisoner Information').
+        and have_link('Edit', href: identification_path(escort)).
+        and have_content('Family name Bigglesworth').
+        and have_content('Forename(s) Tarquin').
+        and have_content('Date of birth 13/02/1972').
+        and have_content('Age 42').
+        and have_content('Sex M').
+        and have_content('Nationality British').
+        and have_content('Prison number A1234BC')
+    end
   end
 end
