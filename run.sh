82956efd9b67e39c3746bfc1fb6c2034901a7712
Chris Wood <chris.wood@digital.justice.gov.uk>
2016-02-10 15:50:45 +0000

    Add drop and create task to Docker run script

--- run.sh
+++ run.sh
@@ -10,6 +10,10 @@ migrate_and_seed)
     echo "running migrate and seed"
     bundle exec rake db:migrate db:seed
     ;;
+drop_and_create)
+    echo "running drop and create"
+    bundle exec rake db:drop db:setup db:seed
+    ;;
 esac
 bundle exec rails server -d --binding 0.0.0.0
 tail -f log/${RAILS_ENV}.log
