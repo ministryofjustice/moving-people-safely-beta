7c4e6bc47b57bef0828ce9ef4dd0f5baeee0a8ce
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-02-10 15:42:23 +0000

    Add aggregate helper methods for feature specs

--- spec/features/summary_page_spec.rb
+++ spec/features/summary_page_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.feature 'Summary page for an escort' do
   scenario 'viewing an escort records overview' do
     travel_to(Date.new(2015, 2, 3)) do
-      escort = create(:escort)
-      visit summary_path(escort)
+      start_escort_form
+      fill_in_identification
+
+      visit summary_path(Escort.last)
 
       expect(page).to have_text('Summary').
         and have_text('Prisoner Information').
-        and have_link('Edit', href: identification_path(escort)).
+        and have_link('Edit', href: identification_path(Escort.last)).
         and have_content('Family name Bigglesworth').
         and have_content('Forename(s) Tarquin').
         and have_content('Date of birth 13/02/1972').
---
+++ spec/support/feature_helpers.rb
@@ -0,0 +1,34 @@
+module FeatureHelpers
+  def start_escort_form
+    visit '/'
+    click_button 'Create new escort record'
+  end
+
+  def fill_in_identification(options = {})
+    options = {
+      prison_number: 'A1234BC',
+      family_name: 'Bigglesworth',
+      forenames: 'Tarquin',
+      sex: 'Male',
+      date_of_birth: Date.civil(1972, 2, 13),
+      nationality: 'British'
+    }.merge(options)
+
+    fill_in 'Prison number', with: options.fetch(:prison_number)
+    fill_in 'Family name', with: options.fetch(:family_name)
+    fill_in 'Forenames', with: options.fetch(:forenames)
+    choose options.fetch(:sex)
+    fill_in 'identification[date_of_birth][day]',
+      with: options.fetch(:date_of_birth)&.day
+    fill_in 'identification[date_of_birth][month]',
+      with: options.fetch(:date_of_birth)&.month
+    fill_in 'identification[date_of_birth][year]',
+      with: options.fetch(:date_of_birth)&.year
+    fill_in 'Nationality', with: options.fetch(:nationality)
+    click_button 'Save'
+  end
+end
+
+RSpec.configure do |c|
+  c.include FeatureHelpers, type: :feature
+end
