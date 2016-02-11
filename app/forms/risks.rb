a0a90425b54d8ff989737a742058f1ad63e44797
Dan Palmer <boss@bitrecursive.com>
2016-02-11 21:33:58 +0000

    Separate risks into general & transit categories
    We need a way to separate the risk categories when
    rendering the risks form. Having a constant list as
    a reference in the form object that can be used in
    the view helps us DRY up our code.

--- app/forms/risks.rb
+++ app/forms/risks.rb
@@ -1,16 +1,11 @@
 class Risks < Form
   include Form::TextToggleAttribute
 
-  text_toggle_attribute :to_self
-  text_toggle_attribute :to_others
-  text_toggle_attribute :violence
-  text_toggle_attribute :from_others
-  text_toggle_attribute :escape
-  text_toggle_attribute :intolerant_behaviour
-  text_toggle_attribute :prohibited_items
-  text_toggle_attribute :disabilities
-  text_toggle_attribute :allergies
-  text_toggle_attribute :non_association
+  GENERAL = %i[ to_self to_others violence from_others escape
+                intolerant_behaviour prohibited_items ].freeze
+  TRANSIT = %i[ disabilities allergies non_association ].freeze
+
+  [GENERAL, TRANSIT].flatten.each(&method(:text_toggle_attribute))
 
   def target
     model.risk_information || model.build_risk_information
