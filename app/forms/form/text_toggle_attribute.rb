7a536c111750f44f44e5cfbbc67838f812746663
Dan Palmer <boss@bitrecursive.com>
2016-02-11 21:30:36 +0000

    Allow nil attributes for Virtus Boolean types
    If a value can’t be coerced into a Boolean by Virtus
    it remains as a string attribute in the form. By checking
    whether the value was coerced correctly we can enforce
    non Boolean values to be nil.

--- app/forms/form/text_toggle_attribute.rb
+++ app/forms/form/text_toggle_attribute.rb
@@ -2,9 +2,15 @@ class Form
   module TextToggleAttribute
     extend ActiveSupport::Concern
 
+    class MaybeBoolean < Virtus::Attribute::Boolean
+      def coerce(value)
+        super if super.in? [true, false]
+      end
+    end
+
     included do
       def self.text_toggle_attribute(attribute_name)
-        attribute attribute_name,              Boolean
+        attribute attribute_name,              MaybeBoolean
         attribute "#{attribute_name}_details", String
       end
     end
