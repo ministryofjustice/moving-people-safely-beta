775f654a9b13773e4db71087665db9cef89d50f2
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-02-12 10:43:06 +0000

    Use validate_inclusion_of matcher in form specs

--- spec/forms/identification_spec.rb
+++ spec/forms/identification_spec.rb
@@ -16,35 +16,6 @@ RSpec.describe Identification, type: :form do
   it_behaves_like 'a form that belongs to an endpoint', 'identification'
   it_behaves_like 'a form with dates', %i[ date_of_birth ]
 
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:prison_number) }
-
-    describe 'sex' do
-      context 'when present' do
-        context 'and included in the list' do
-          it 'is valid' do
-            subject.sex = 'male'
-            subject.valid?
-            expect(subject.errors[:sex]).to be_empty
-          end
-        end
-
-        context 'and not included in the list' do
-          it 'is not valid' do
-            subject.sex = 'camel'
-            subject.valid?
-            expect(subject.errors[:sex]).to_not be_empty
-          end
-        end
-      end
-
-      context 'when not present' do
-        it 'is valid' do
-          subject.sex = nil
-          subject.valid?
-          expect(subject.errors[:sex]).to be_empty
-        end
-      end
-    end
-  end
+  it { is_expected.to validate_presence_of(:prison_number) }
+  it { is_expected.to validate_inclusion_of(:sex).in_array(%w[ male female ]) }
 end
