b20c93285fcc5187c6e5383b7c261f3f08212ee7
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-02-16 16:13:53 +0000

    Add error_wrapper view helper
    This helper creates a content div with the correct style, based
    on the presence of errors on the form passed. We will use this
    on the search form in the home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def error_wrapper(form, styles:, styles_on_error:, &_blk)
+    if form.object.errors.any?
+      styles = [styles, styles_on_error].join(' ')
+    end
+    content_tag(:div, class: styles) { yield }
+  end
+
   def navigation_item(text, path)
     content_tag(:li, link_or_text(text, path), class: apply_current_class(path))
   end
