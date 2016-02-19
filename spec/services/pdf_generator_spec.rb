10807e9eb25043b17ef45b69fd734c3b035d49ba
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-02-19 15:35:25 +0000

    Add Not For Release section

--- spec/services/pdf_generator_spec.rb
+++ spec/services/pdf_generator_spec.rb
@@ -17,10 +17,7 @@ RSpec.describe PdfGenerator, type: :service do
     let(:html) { described_class.render(escort) }
     it 'generates the correct HTML code' do
       travel_to(Date.new(2015, 2, 3)) do
-        expect(html).to have_content('Master').
-          and have_content('Cover sheet').
-          and have_content('Date of travel').
-          and have_content('Official - Sensitive').
+        expect(html).to have_content('Person Escort Record - cover sheet').
           and have_content('Family name Bigglesworth').
           and have_content('Forenames Tarquin').
           and have_content('Date of birth 13 2 1972').
@@ -28,7 +25,9 @@ RSpec.describe PdfGenerator, type: :service do
           and have_content('Sex M').
           and have_content('Prison number A1234BC').
           and have_content('Nationality British').
-          and have_content('Attach photo if available')
+          and have_content('Attach photo').
+          and have_content('Not for release').
+          and have_content('Reason')
       end
     end
   end
