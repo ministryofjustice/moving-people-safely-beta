5a34bb01434d17d9d6bb2e63786d060ccf1f0bf2
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-02-22 17:37:44 +0000

    Add front page markers
    Introduces 'Yes'/'No' pattern for Risks, Offence and Healthcare
    information on the cover sheet.

--- spec/services/pdf_generator_spec.rb
+++ spec/services/pdf_generator_spec.rb
@@ -15,23 +15,29 @@ RSpec.describe PdfGenerator, type: :service do
 
   describe '.render' do
     let(:html) { described_class.render(escort) }
-    it 'generates the correct HTML code' do
+    it 'generates the correct HTML for the header' do
+      expect(html).to have_content('Person Escort Record - cover sheet')
+    end
+    it 'generates the correct HTML for NFR section' do
+      expect(html).to have_content('Not for release').
+        and have_content('Reason')
+    end
+    it 'generates the correct HTML for move information section' do
+      expect(html).to have_content('From').
+        and have_content('To').
+        and have_content('Date of travel').
+        and have_content('Destination update')
+    end
+    it 'generates the correct HTML for personal information section' do
       travel_to(Date.new(2015, 2, 3)) do
-        expect(html).to have_content('Person Escort Record - cover sheet').
-          and have_content('Family name Bigglesworth').
+        expect(html).to have_content('Family name Bigglesworth').
           and have_content('Forenames Tarquin').
           and have_content('Date of birth 13 2 1972').
           and have_content('Age 42').
           and have_content('Sex M').
           and have_content('Prison number A1234BC').
           and have_content('Nationality British').
-          and have_content('Attach photo').
-          and have_content('Not for release').
-          and have_content('Reason').
-          and have_content('From').
-          and have_content('To').
-          and have_content('Date of travel').
-          and have_content('Destination update')
+          and have_content('Attach photo')
       end
     end
   end
