3a70c854767830b29afcc90b4b4ea7e3c4d19768
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-02-24 17:58:53 +0000

    Reduce the routes generated from devise
    We do not want to offer registration functionality for
    the time being.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[ registrations ]
+
   scope 'escort/:id' do
     resource :identification,
       only: %i[ show update ],
