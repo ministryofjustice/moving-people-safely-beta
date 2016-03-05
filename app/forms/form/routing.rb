3203b360371063d66037910738f7a314b1d53cdd
Dan Palmer <boss@bitrecursive.com>
2016-03-05 21:37:55 +0000

    DRY form routes
    Provide a central place for form names to be managed
    for both routing and the navigation menu.
    
    We rely on manually updating this list when forms are
    added or removed in order to maintain a specific order
    for the navigation menu, otherwise grepping the
    ObjectSpace for form objects would’ve been a neater
    solution.

---
+++ app/forms/form/routing.rb
@@ -0,0 +1,21 @@
+class Form
+  module Routing
+    FORM_NAMES = %i[ identification risks move_information ]
+
+    FormData = Struct.new(:name) do
+      def path
+        name.to_s.dasherize
+      end
+    end
+
+  module_function
+
+    def resource_list
+      form_names.map { |name| FormData.new(name) }
+    end
+
+    def form_names
+      FORM_NAMES
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,20 @@ Rails.application.routes.draw do
   devise_for :users, skip: %i[ registrations ]
 
   scope 'escort/:id' do
-    resource :identification,
-      only: %i[ show update ],
-      controller: :escorts,
-      page: :identification
-
-    resource :risks,
-      only: %i[ show update ],
-      controller: :escorts,
-      page: :risks
-
-    resource :move_information,
-      only: %i[ show update ],
-      controller: :escorts,
-      path: 'move-information',
-      page: :move_information
+    Form::Routing.resource_list.each do |r|
+      resource r.name,
+        only: %i[ show update], controller: :escorts,
+        path: r.path, page: r.name
+    end
 
     get 'summary', controller: :escorts
 
     resource :pdf, only: :show
   end
+
   resource :escort, only: :create
+
   post 'search', controller: :home_pages
+
   root to: 'home_pages#show'
 end
