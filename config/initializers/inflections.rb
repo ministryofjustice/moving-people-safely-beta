05e7d74355f28f9ed1a810ccccd8c7951043865b
Dan Palmer <boss@bitrecursive.com>
2016-03-10 21:32:37 +0000

    Introduce a healthcare form to the user flow

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -16,6 +16,5 @@
 # end
 
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.irregular 'risk_information', 'risk_information'
-  inflect.irregular 'health_information', 'health_information'
+  inflect.uncountable %w[ health_information risk_information ]
 end
