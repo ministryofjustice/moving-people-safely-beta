ce507da497251bb795322ddf7b53aab4354c5422
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-03-17 15:38:04 +0000

    Add form helpers and error container
    Introduces:
    
    - date and single field form helpers
    - generic 'error_container' helper (to manage error classes)
    - provides an ID to link show/hide elements them to error messages

--- app/helpers/form_elements_helper.rb
+++ app/helpers/form_elements_helper.rb
@@ -1,21 +1,23 @@
 module FormElementsHelper
-  def date_fields(form, name, &blk)
-    error_container(form, name) {
-      capture(&blk)
-    }
+  def date_fields(form, field)
+    error_container(form, field, 'form-date') do
+      yield
+    end
   end
 
-  def error_container(form, name, options = { class: 'form-date group' }, &blk)
-    html_class = [options[:class]]
-    if form.object.errors.include?(name)
-      html_class << ' error'
+  def single_field(form, field)
+    error_container(form, field) do
+      form.label(field, class: 'form-label-bold', for: field) +
+        form.text_field(field, class: 'string form-control', id: field)
     end
+  end
+
+  def error_container(form, field, html_class = nil)
     content_tag(
       :div,
-      class: html_class,
-      id: "#{form.object.name}_#{name}",
-      &blk
-    )
+      class: html_classes(form, field, html_class),
+      id: "#{form.object.name}_#{field}"
+    ) { yield }
   end
 
   def form_group_container(classes:'', &_blk)
@@ -29,5 +31,11 @@ module FormElementsHelper
 
   def join(*strings)
     strings.flatten.inject(ActiveSupport::SafeBuffer.new, &:<<)
+  end
+
+  def html_classes(form, field, html_class)
+    html_classes = ['form-group', html_class]
+    html_classes << 'error' if form.object.errors.include?(field)
+    html_classes.join(' ')
   end
 end
