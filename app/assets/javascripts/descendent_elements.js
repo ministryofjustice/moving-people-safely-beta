b6ffb5b0c0e18779277913bd675b56f23f8cbeb4
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-03-29 18:47:49 +0100

    Allow exclusion of descendentElement checkboxes
    An interim measure while we re-evaluate our approach to generating
    descendentElement checkboxes - that is those where visibility of
    supplemental element(s) are dictated by a specific input state.
    
    The previous implementation of descendentElements was based on:
    
    1. There being only one set of checkboxes per block, each of which is
    related to the descendent element
    
    2. There being no 'nesting' of elements
    
    During this sprint the requirement has changed and we will be changing
    our approach to there generation of HTML for these elements. In light of
    this it would be unwise to alter the HTML at this time and I have
    therefore amended the JavaScript to allow developers to pass a number of
    IDs to the plugin which will be ignored.
    
    This interim measure is non-optimal and I expect the final code to be
    substantially more succinct.

--- app/assets/javascripts/descendent_elements.js
+++ app/assets/javascripts/descendent_elements.js
@@ -2,7 +2,7 @@
 
 (function ($) {
 
-    var trueChecked, falseChecked, manageState, setVisibility;
+    var trueChecked, falseChecked, manageState, setVisibility, changeIgnoredIds;
 
     trueChecked = function ($block) {
         return $block.find('[id$=_true]:checked').length == 1;
@@ -28,16 +28,37 @@
         setVisibility(showUnknownLabel, $unknownLabel);
     };
 
-    $.fn.descendentElements = function () {
+    changeIgnoredIds = function (ids) {
+        var id, newId;
+        for (var i = 0; i < ids.length; i++) {
+            id = ids[i];
+            newId = id + '_no_events';
+            $('#' + id).attr('id', newId);
+            $('[for=' + id).attr('for', newId);
+        }
+    };
+
+    $.fn.descendentElements = function (options) {
+
+        var settings = $.extend({}, $.fn.descendentElements.defaults, options);
+
+        changeIgnoredIds(settings.ignored_checkboxes);
+
         return this.each(function () {
             var $this = $(this);
+
             manageState($this);
+
             $this.on('change', function (e) {
                 manageState($this);
             })
         })
     };
 
+    $.fn.descendentElements.defaults = {
+        ignored_checkboxes: ''
+    }
+
 }(jQuery));
 
-$('.js_has_optional_section').descendentElements();
+$('.js_has_optional_section').descendentElements({ignored_checkboxes: ['risks_open_acct_true', 'risks_open_acct_false', 'risks_open_acct_unknown']});
