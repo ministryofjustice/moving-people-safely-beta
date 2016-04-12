e8066a582e987a0631c375c3f0bcbd5f46029a75
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-04-12 16:10:47 +0100

    Update cover sheet to reflect new design
    Several updates to the presentation of information on the
    front page in response to design changes made this sprint.

--- app/presenters/pdf/prisoner_presenter.rb
+++ app/presenters/pdf/prisoner_presenter.rb
@@ -4,11 +4,15 @@ module Pdf
       @model = model
     end
 
-    delegate :family_name, :forenames, :prison_number, :nationality,
-      :capitalized_sex, :date_of_birth, :age, :cro_number, :pnc_number,
+    delegate :prison_number, :nationality, :capitalized_sex,
+      :date_of_birth, :age, :cro_number, :pnc_number,
       to: :@model
 
     delegate :day, :month, :year,
       to: :date_of_birth, prefix: true, allow_nil: true
+
+    def names
+      [@model.family_name, @model.forenames].reject(&:blank?).join(' \ ')
+    end
   end
 end
