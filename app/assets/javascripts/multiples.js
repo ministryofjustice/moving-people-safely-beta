92fc78a5d0e7662c76356c8e298819351fff63b5
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-04-12 12:10:53 +0100

    Introduce multiples plugin
    Introduces a jQuery plugin written for this application which
    manages the display, ARIA attributes and interactions for multiples
    within the MPS project.

---
+++ app/assets/javascripts/multiples.js
@@ -0,0 +1,111 @@
+'use strict';
+
+(function ($) {
+
+  var insertShowButton, setUpEvents, showElementsWithShowClass,
+    hideItem, showItem, showFirstHiddenItem, manageAddLink, init;
+
+  init = function ($el, settings) {
+    insertShowButton($el, settings);
+
+    $el.find('.' + settings.itemClass).each(function () {
+      var $this = $(this);
+      setUpEvents($this, settings);
+      showElementsWithShowClass(settings);
+    })
+  };
+
+  hideItem = function ($el) {
+    $el.removeClass('shown');
+    $el.attr('aria-hidden', true);
+    $el.hide();
+  };
+
+  showItem = function ($el) {
+    $el.addClass('shown');
+    $el.attr('aria-hidden', false);
+    $el.find('[value="_destroy"]')
+      .prop('checked', false)
+      .removeAttr('checked');
+    $el.show();
+  };
+
+  setUpEvents = function ($el) {
+    $el.on('change', function (e) {
+      var $item, $input;
+      $item = $(e.currentTarget);
+      $input = $(e.target);
+
+      if ($input.attr('value') == "_destroy") {
+        hideItem($item);
+      }
+    })
+  };
+
+  manageAddLink = function ($el) {
+    var $numberOfItems, $numberOfShownItems;
+    $numberOfItems = $el.find('.item').length;
+    $numberOfShownItems = $el.find('.item.shown').length;
+
+    if ($numberOfItems == $numberOfShownItems) {
+      $('.add-another', $el).hide();
+    } else {
+      $('.add-another', $el).show();
+    }
+  };
+
+  insertShowButton = function ($el, settings) {
+    var $button = $('<button>', {
+      text: settings.addAnotherText,
+      class: settings.addAnotherClass
+    });
+    $el.append($button);
+  };
+
+  showElementsWithShowClass = function (settings) {
+    $('.' + settings.itemClass).each(function () {
+      var $this = $(this);
+      if ($this.hasClass(settings.shownClass)) {
+        showItem($this);
+      } else {
+        hideItem($this);
+      }
+    })
+  };
+
+  showFirstHiddenItem = function ($el) {
+    var $hiddenItems = $el.find('.item:not(.shown)');
+    if ($hiddenItems.length > 0) {
+      showItem($hiddenItems.first());
+    }
+  };
+
+  $.fn.multiples = function (options) {
+    var settings = $.extend({}, $.fn.multiples.defaults, options);
+    return this.each(function () {
+      var $this = $(this);
+      init($this, settings);
+      $this.on('click', '.add-another', function (e) {
+        var $container = $(e.delegateTarget),
+          $button = $(e.target);
+        e.preventDefault();
+        showFirstHiddenItem($container);
+        manageAddLink($container);
+      });
+      $this.on('change', function () {
+        manageAddLink($this);
+      });
+    })
+  };
+
+  $.fn.multiples.defaults = {
+    addAnotherText: 'Add another',
+    addAnotherClass: 'add-another',
+    itemClass: 'item',
+    shownClass: 'shown'
+  };
+
+})(jQuery);
+
+
+$('.multiples-container').multiples();
